62fd9cde10660540d90b1da5e2c1095ed3051770
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-07-30 11:36:16 +0200

    derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ use maplike::containers::Container;
 use maplike::ops::{Clear, Get, Insert, Modify, Put, Remove, WithOne};
 
 /// Many-to-many bidirectional map made of two antiparallel maps.
+#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct MultiBimap<L2R, R2L> {
     left_to_right: L2R,
     right_to_left: R2L,
