5a1a1eca0db1f2c4fcfc8a6b3c3350bfaa985d23
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-07-30 13:04:58 +0200

    Add feature flag documentation using `document-features` crate

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@
 
 #![doc(html_root_url = "https://docs.rs/multi_bimap")]
 #![doc = include_str!("../README.md")]
+#![cfg_attr(docsrs, doc = "\n## Feature flags\n")]
+#![cfg_attr(docsrs, doc = document_features::document_features!())]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 //#![deny(missing_docs)] // Temporary until I fix this missing docs `undoredo` derives.
 #![forbid(unsafe_code)]
 //#![no_std]
